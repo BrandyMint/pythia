c00126ae5dd229f0a70438a67c3f561ee4dbb52c
eka <evenKorobkov@gmail.com>
2012-05-16 15:15:06 +0400

    start rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pythia::Application.config.secret_token = 'f748842cf4a0d56f99fe41ab772f10e1e24a3f6e56345f95c3d9f39f4d6ce444d74179253abae6c25c43383af2d3ff7d57c1b95a1c0572d0802ac2e9b3dbd339'
