705831fd61c4f68fd98dfc6f1cd36b42dc3873e7
eka <evenKorobkov@gmail.com>
2012-05-18 23:30:53 +0400

    add parse subscribe.ru/business

--- lib/RssSource.rb
+++ lib/RssSource.rb
@@ -1,14 +1,27 @@
 class RssSource
-    def self.update()
-        require 'nokogiri'
-        require 'open-uri'
-        url = "http://cat.yandex.ru/yca/cat/Business"
-        (1..9).each do |page|
-            doc = Nokogiri::HTML(open(url))
-            doc.css('h3.b-result__head a.b-result__name').each {|link| puts link.attr("href")} 
-            url = "http://cat.yandex.ru/yca/cat/Business/#{page}.html"
+  def self.update
+    require "open-uri"
+    require "nokogiri"
+    url = "http://subscribe.ru/catalog/business?rss"
+    id_next_page = 201
+    is_end = false
+    while not is_end
+      begin
+        doc = Nokogiri::HTML(open(url))
+      rescue Exception => e
+        is_end = true
+      end
+      is_rss = 0
+      doc.css('span.lightblue a').each do |rss|
+        is_rss = (is_rss + 1) % 2
+        if is_rss == 0
+          p rss.text  #for testing. add to model
         end
+      end
+      url = "http://subscribe.ru/catalog/business?rss&pos=#{id_next_page}"
+      id_next_page += 200
     end
+  end
 end
 
 RssSource.update
