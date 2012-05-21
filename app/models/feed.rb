f16c54b31a6f167cbc05600adf0a672b304275b5
eka <evenKorobkov@gmail.com>
2012-05-21 18:25:17 +0400

    RssSource доступен через rails консоль, заполняя модель Feed (name, url) где имя - имя сайта, url - ссылка на rsss канал

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,4 @@
 class Feed < ActiveRecord::Base
   attr_accessible :name, :url
+  belongs_to :source
 end
--- app/models/source.rb
+++ app/models/source.rb
@@ -1,3 +1,4 @@
 class Source < ActiveRecord::Base
   attr_accessible :name, :url
+  has_many :feeds
 end
