01bdfceb6c2e27aede4d331de9e859dfb1acca68
eka <evenKorobkov@gmail.com>
2012-05-22 18:15:54 +0400

    [#29979199] Модель SubscribeSource наследуем от Source. В Source добавляем поле type. Логику импорта фидов перенести из рейк-задачи в модель SubscribeSource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120516174203) do
+ActiveRecord::Schema.define(:version => 20120522134141) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(:version => 20120516174203) do
     t.string   "url"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "type"
   end
 
 end
