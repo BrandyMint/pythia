fa9a31a71d8361e983a10b1c88f069547601f007
eka <evgenKorobkov@gmail.com>
2012-07-12 16:16:40 +0400

    [#29878615] валидация у company_mention

--- app/models/company_mention.rb
+++ app/models/company_mention.rb
@@ -3,12 +3,8 @@ class CompanyMention < ActiveRecord::Base
   belongs_to :company
   belongs_to :article
   
-  # TODO validation
-  
+  validates :company, :null => false
+  validates :article, :null => false
 
-  ## это еще зачем? есть же scope arcicles.last_week его и нужно исопльзовать
-  #def last_week_articles
-  #  articles.last_week
-  #end
-  
+    
 end
