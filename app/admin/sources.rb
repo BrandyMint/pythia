c9a179e2d27983fdc80dc42168e8674e45922715
eka <evgenKorobkov@gmail.com>
2012-08-02 14:10:59 +0400

    [#32977347] можно добавить Source в админке

--- app/admin/sources.rb
+++ app/admin/sources.rb
@@ -1,3 +1,12 @@
+# coding: utf-8
 ActiveAdmin.register Source do
-  
+
+  form do |f|
+    f.inputs "Source" do
+      f.input :name 
+      f.input :url 
+      f.input :type
+    end
+    f.buttons
+  end
 end
