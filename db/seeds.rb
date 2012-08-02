8ad432a1a700f5049e31160ed21a7b8040be0798
eka <evgenKorobkov@gmail.com>
2012-08-02 15:07:47 +0400

    [##32977347] Source::Manual >> seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Source.create :name => :SubscriberRu, :url => 'http://ya.ru', :type => 'Source::SubscriberRu'
-Source.delete_all
-Feed.delete_all
 
-Source::SubscriberRu.create :name=>:SubscriberRu, :url=>'http://news.yandex.ru/business.rss'
+Source::Manual.create :name => :ManualRss, url: 'http://icfdev.ru' if Source.find_by_type 'Source::Manual'
+Source::SubscriberRu.create :name=>:SubscriberRu, :url=>'http://news.yandex.ru/business.rss' if Source.find_by_type 'Source::SubscriberRu'
 Feed::Rss.create :name=>:Rss_yandex, :url=>'http://news.yandex.ru/business.rss'
