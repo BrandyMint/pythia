d3b56eafcd452b6e166fd70ebbcd78d8a715c0fc
eka <evgenKorobkov@gmail.com>
2012-08-06 15:47:54 +0400

    [#33915353] remove seed/feed.rb (seed.rb)

--- db/seeds/sources.rb
+++
@@ -1,2 +0,0 @@
-Source.create(:id => 1, :name => "SubscriberRu", :url => "http://news.yandex.ru/business.rss", :type => "Source::SubscriberRu")
-Source.create(:id => 2, :name => "ManualRss", :url => "http://icfdev.ru", :type => "Feed::Rss")
