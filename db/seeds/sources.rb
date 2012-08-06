0179bbafb49477907b5cd688dacbc1715563a9fe
eka <evgenKorobkov@gmail.com>
2012-08-06 15:07:43 +0400

    [#33922611] seed из pythia@icfdev.ru

--- db/seeds/sources.rb
+++ db/seeds/sources.rb
@@ -1,1 +1,2 @@
-Source.create(:id => 5, :name => "subscriberRu", :url => "http://yandex.ru", :type => "Source::SubscriberRu")
+Source.create(:id => 1, :name => "SubscriberRu", :url => "http://news.yandex.ru/business.rss", :type => "Source::SubscriberRu")
+Source.create(:id => 2, :name => "ManualRss", :url => "http://icfdev.ru", :type => "Feed::Rss")
