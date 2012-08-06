977eb16edea35507487709eada2566288544dc50
eka <evgenKorobkov@gmail.com>
2012-08-06 18:09:29 +0400

    [#33927455] Seed.rb

--- app/models/feed/rss.rb
+++ app/models/feed/rss.rb
@@ -3,15 +3,19 @@ require 'open-uri'
 require 'feedzirra'
 class Feed::Rss < Feed
 
-  # before_validation do
-  #   # puts "Feed:validate #{url}"
-  #   # self.source ||= Source.find_by_name 'manual'
-  #   # self.name ||= dom.title
-  #   true
-  # end
+  before_validation do
+    # puts "Feed:validate #{url}"
+    # self.source ||= Source.find_by_name 'manual'
+    # self.name ||= dom.title
+    # переписать!
+    lenta  = Feedzirra::Feed.fetch_and_parse(url)
+    self.name = lenta.title
+
+  end
 
   def collect_articles
     lenta = Feedzirra::Feed.fetch_and_parse(url)
+    self.name = lenta.title
     if lenta
       lenta.entries.each do |msg|
         articles.create(:title => msg.title, :url => msg.url, :text => msg.summary, :guid => msg.entry_id)
@@ -22,3 +26,9 @@ class Feed::Rss < Feed
     end
   end
 end
+
+
+=begin
+  1.9.3p125 :021 > lenta.entries.first
+  <Feedzirra::Parser::RSSEntry:0x0000000724bf88 @title="Медведев спустился в угольную шахту \"Листвяжная\"", @url="http://news.yandex.ru/yandsearch?cl4url=www.rosbalt.ru%2Ffederal%2F2012%2F08%2F06%2F1019407.html&cat=6&lang=ru", @summary="Глава российского правительства Дмитрий Медведев в понедельник проведет совещание по развитию угольной промышленности в Ленинск-Кузнецком Кемеровской области, а также посетит шахту &quot;Комсомолец&quot;, на которой в конце июля произошел пожар, сообщили в пресс-службе кабинета министров.", @published=2012-08-06 13:05:19 UTC, @entry_id="http://news.yandex.ru/yandsearch?cl4url=www.rosbalt.ru%2Ffederal%2F2012%2F08%2F06%2F1019407.html&cat=6&lang=ru"> 
+=end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,33 +3,42 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#  cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#  Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Source.create :name => :SubscriberRu, :url => '"http://ya.ru', :type => 'Source::SubscriberRu'
+# Source.create :name => :SubscriberRu, :url => 'http://ya.ru', :type => 'Source::SubscriberRu'
 
-Source::Manual.create :name => :ManualRss, url: '"http://icfdev.ru' if Source.find_by_type 'Source::Manual'
-Source::SubscriberRu.create :name=>:SubscriberRu, :url=>'"http://news.yandex.ru/business.rss' if Source.find_by_type 'Source::SubscriberRu'
-Feed::Rss.create url: "http://news.yandex.ru/business.rss"
-Feed::Rss.create url: "http://ria.ru/export/rss2/economy/index.xml"
-Feed::Rss.create url: "http://www.vesti.ru/section.rss?cid=6"
-Feed::Rss.create url: "http://www.rusnovosti.ru/rss/economics.xml"
-Feed::Rss.create url: "http://www.gazeta.ru/export/rss/financial.xml"
-Feed::Rss.create url: "http://feeds.newsru.com/com/www/section/finance"
-Feed::Rss.create url: "http://news.rambler.ru/rss/popular/economics/"
-Feed::Rss.create url: "http://www.vz.ru/rss.xml"
-Feed::Rss.create url: "http://lenta.ru/rss/news/business/"
-Feed::Rss.create url: "http://lenta.ru/rss/news/finance/"
-Feed::Rss.create url: "http://lenta.ru/rss/news/economy"
-Feed::Rss.create url: "http://news.mail.ru/rss/economics/"
-Feed::Rss.create url: "http://feeds.feedburner.com/skyline2"
-Feed::Rss.create url: "http://www.vestifinance.ru/yandex.xml"
-Feed::Rss.create url: "http://static.feed.rbc.ru/rbc/logical/rbcdaily.ru/daily.rss"
-Feed::Rss.create url: "http://feeds.feedburner.com/profi-forex/PYBo"
-Feed::Rss.create url: "http://www.bfm.ru/news/rss/articles/"
-Feed::Rss.create url: "http://expert.ru/doc-list/rss/"
-Feed::Rss.create url: "http://expert.ru/doc-list/rss/?tags=3"
-Feed::Rss.create url: "http://www.ng.ru/ng.rss"
-Feed::Rss.create url: "http://www.rg.ru/xml/index.xml"
-Feed::Rss.create url: "http://www.fontanka.ru/fontanka.rss"
-Feed::Rss.create url: "http://www.bbc.co.uk/russian/index.xml"
+Source::Manual.create name: 'ManualRss', url: 'http://yandex.ru'
+Source::SubscriberRu.create :name=>:SubscriberRu, :url=>'"http://news.yandex.ru/business.rss'
+=begin
+ name: "news.yandex.ru", 
+ url: "http://news.yandex.ru/business.rss", 
+ source_id: 22, 
+ type: nil, 
+ articles_count: 0
+=end
+
+
+Feed::Rss.create url: "http://news.yandex.ru/business.rss", source_id: 22
+Feed::Rss.create url: "http://ria.ru/export/rss2/economy/index.xml", source_id: 22
+Feed::Rss.create url: "http://www.vesti.ru/section.rss?cid=6", source_id: 22
+Feed::Rss.create url: "http://www.rusnovosti.ru/rss/economics.xml", source_id: 22
+Feed::Rss.create url: "http://www.gazeta.ru/export/rss/financial.xml", source_id: 22
+Feed::Rss.create url: "http://feeds.newsru.com/com/www/section/finance", source_id: 22
+Feed::Rss.create url: "http://news.rambler.ru/rss/popular/economics/", source_id: 22
+Feed::Rss.create url: "http://www.vz.ru/rss.xml", source_id: 22
+Feed::Rss.create url: "http://lenta.ru/rss/news/business/", source_id: 22
+Feed::Rss.create url: "http://lenta.ru/rss/news/finance/", source_id: 22
+Feed::Rss.create url: "http://lenta.ru/rss/news/economy", source_id: 22
+Feed::Rss.create url: "http://news.mail.ru/rss/economics/", source_id: 22
+Feed::Rss.create url: "http://feeds.feedburner.com/skyline2", source_id: 22
+Feed::Rss.create url: "http://www.vestifinance.ru/yandex.xml", source_id: 22
+Feed::Rss.create url: "http://static.feed.rbc.ru/rbc/logical/rbcdaily.ru/daily.rss", source_id: 22
+Feed::Rss.create url: "http://feeds.feedburner.com/profi-forex/PYBo", source_id: 22
+Feed::Rss.create url: "http://www.bfm.ru/news/rss/articles/", source_id: 22
+Feed::Rss.create url: "http://expert.ru/doc-list/rss/", source_id: 22
+Feed::Rss.create url: "http://expert.ru/doc-list/rss/?tags=3", source_id: 22
+Feed::Rss.create url: "http://www.ng.ru/ng.rss", source_id: 22
+Feed::Rss.create url: "http://www.rg.ru/xml/index.xml", source_id: 22
+Feed::Rss.create url: "http://www.fontanka.ru/fontanka.rss", source_id: 22
+Feed::Rss.create url: "http://www.bbc.co.uk/russian/index.xml", source_id: 22
