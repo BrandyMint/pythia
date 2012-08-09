131e3b463feacbe7500b1ebb46a79238c6b49588
eka <evgenKorobkov@gmail.com>
2012-08-09 13:22:16 +0400

    [#34091943]companyMention fix

--- app/admin/company_mentions.rb
+++ app/admin/company_mentions.rb
@@ -1,24 +1,14 @@
 ActiveAdmin.register CompanyMention do
 
   index do
-    column "Company" do |company_mention|
+    column :company, :sortable => :company_id do |company_mention|
       link_to company_mention.company, admin_company_path(company_mention.company)
-    end  
-
-    column "Article" do |company_mention|
+    end
+    column :article, :sortable => :article_id do |company_mention|
       link_to company_mention.article, admin_article_path(company_mention.article)
     end
+
+    default_actions
   end
-
-  # index do
-  #   column :company, :sortable => :company_id do
-  #     link_to company_mentions.company, admin_company_path(company_mentions.company)
-  #   end
-  #   column :article, :sortable => :article_id do
-  #     link_to company_mentions.article, admin_article_path(company_mentions.article)
-  #   end
-
-  #   default_actions
-  # end
 
 end
